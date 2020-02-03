a6ace5b0cd1e5135585e50e9c7fa02f902b6c495
arisa <arisankochira10@gmail.com>
2020-02-03 11:23:51 +0900

    fix

--- insert.php
+++ insert.php
@@ -4,29 +4,60 @@
 define('DSN', 'mysql:host=mysql;dbname=pet_shop;charset=utf8;');
 define('USER', 'staff');
 define('PASSWORD', '9999');
-
 // DBに接続
 try {
   $dbh = new PDO(DSN, USER, PASSWORD);
-  echo '本日のご紹介ペット！' . '<br>';
 } catch (PDOException $e) {
   // 接続がうまくいかない場合こちらの処理がなされる
   echo $e->getMessage();
   exit;
 }
-
-// SQL文の組み立て
-$sql2 = "select * from animals";
-
-// プリペアドステートメントの準備
-$stmt = $dbh->prepare($sql2);
-
-// プリペアドステートメントの実行
-$stmt->execute();
-
-// 結果の受け取り
-$row = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-foreach ($row as $animal) {
-  echo $animal['type'].'の'. $animal['classifcation'] . 'ちゃん'.'<br>'. $animal['description'].'<br>'.$animal['birthday'].'  生まれ'.'<br>'.'出身地  '.$animal['birthplace'].'<hr>';
+// 新規タスクの追加にリクエスとメソットでGETの指示
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+  // 連想配列でゲット変数を設定
+  $keyword = $_GET['keyword'];
+  if ($keyword == '') {
+    $sql = "select * from animals";
+    $stmt = $dbh->prepare($sql);
+  } else {
+    $sql = "select * from animals where description like :keyword";
+    $stmt = $dbh->prepare($sql);
+    // PDO処理
+    $keyword_param = "%" . $keyword . "%";
+    $stmt->bindParam(":keyword", $keyword_param);
+  }
+  $stmt->execute();
+  // 結果の受け取り
+  $animals = $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
+?>
+<!DOCTYPE html>
+<html lang="ja">
+​
+<head>
+  <meta charset="UTF-8">
+  <title>ペットショップ</title>
+</head>
+​
+<body>
+  <h2>本日のご紹介ペット！</h2>
+  <p>
+    <form action="" method="get">
+      <label for="keyword">キーワード:
+        <input type="text" name="keyword" placeholder="キーワードの入力">
+      </label>
+      <input type="submit" value="検索">
+    </form>
+    <br>
+    <?php foreach ($animals as $animal) : ?>
+      <?php
+        echo $animal['type'] . 'の' . $animal['classifcation'] . 'ちゃん' . '<br>' . $animal['description'] .
+        '<br>' . $animal['birthday'] . ' 生まれ' .
+        '<br>' . '出身地 ' . $animal['birthplace'];
+      ?>
+      <hr>
+    <?php endforeach; ?>
+  </p>
+</body>
+​
+</html>
